aa2db56b0a2fb809e420bbfa96e426562507bc10
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-07-02 12:12:43 +0900

    Time: 9 ms (36.68%), Space: 45.1 MB (43.07%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int count = 0;
+        int i = 0, j = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while(i < g.length && j < s.length){
+            if(s[j] >= g[i]){
+                count++;
+                j++; i++;
+            }
+            else
+                j++;
+        }
+        
+        return count;
+    }
+}
